1c0a026d55e726fc11e1eba70ed66a0a8a69eeaf
tushar-chadha <tusharchadha26@gmail.com>
2024-03-23 14:02:08 +0530

    created staggered tiles

--- lib/visuals/screens/productByCat.dart
+++ lib/visuals/screens/productByCat.dart
@@ -1,16 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:gap/gap.dart';
+import 'package:shop/helper/helperService.dart';
 import 'package:shop/model/sneakerModel.dart';
+import 'package:shop/visuals/shared/homeWidget.dart';
+import 'package:shop/visuals/shared/productCard.dart';
+import 'package:shop/visuals/shared/staggeredTile.dart';
 import 'package:shop/visuals/shared/tabBar.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
 
 class ProductByCat extends StatefulWidget {
   const ProductByCat({super.key});
-
-  // final Future<List<Sneaker>> male, kid, female;
-
-  // const ProductByCat(
-  //     {super.key, required this.male, required this.kid, required this.female});
 
   @override
   State<ProductByCat> createState() => _ProductByCatState();
@@ -18,112 +21,139 @@ class ProductByCat extends StatefulWidget {
 
 class _ProductByCatState extends State<ProductByCat>
     with TickerProviderStateMixin {
+  late Future<List<Sneaker>> _male, _female, _kid;
+
+  void getMaleShoe() {
+    _male = helper().getMaleShoesDataFromJson();
+  }
+
+  void getfemaleShoe() {
+    _female = helper().getWomenShoesDataFromJson();
+  }
+
+  void getKidShoe() {
+    _kid = helper().getKidsShoesDataFromJson();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getMaleShoe();
+    getfemaleShoe();
+    getKidShoe();
+  }
+
   @override
   Widget build(BuildContext context) {
     TabController ShoeMenuController = TabController(length: 3, vsync: this);
     return Scaffold(
       backgroundColor: const Color(0xFFE2E2E2),
-      body: Stack(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.4,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                filterQuality: FilterQuality.high,
-                image: AssetImage("assets/images/top_image.png"),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.4,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  filterQuality: FilterQuality.high,
+                  image: AssetImage("assets/images/top_image.png"),
+                ),
               ),
             ),
-          ),
-          SafeArea(
-            right: false,
-            left: false,
-            bottom: false,
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Icon(
-                          AntDesign.close,
-                          color: Colors.white,
-                          size: 30,
-                        ),
-                      ),
-                      const Icon(
-                        AntDesign.filter,
+            SafeArea(
+              right: false,
+              left: false,
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Icon(
+                        AntDesign.close,
                         color: Colors.white,
                         size: 30,
                       ),
-                    ],
-                  ),
-                  Gap(10),
-                  tabBarWidget(
-                    controller: ShoeMenuController,
-                  ),
-                  const Gap(20),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height,
-                    child: TabBarView(
-                      controller: ShoeMenuController,
-                      children: [
-                        Container(
-                          height: MediaQuery.of(context).size.height,
-                          child: SingleChildScrollView(
-                              child: Column(
-                            children: List.generate(
-                              20,
-                              (index) => Container(
-                                margin: const EdgeInsets.all(1),
-                                color: Colors.blue,
-                                width: 10,
-                                height: 10,
-                              ),
-                            ),
-                          )),
-                        ),
-                        Container(
-                          height: MediaQuery.of(context).size.height,
-                          // child: ListView(
-                          //   children: List.generate(
-                          //     20,
-                          //     (index) => Container(
-                          //       margin: const EdgeInsets.all(10),
-                          //       color: Colors.blue,
-                          //       width: 10,
-                          //       height: 10,
-                          //     ),
-                          //   ),
-                          // ),
-                        ),
-                        Container(
-                          height: MediaQuery.of(context).size.height,
-                          child: ListView(
-                            children: List.generate(
-                                20,
-                                (index) => Container(
-                                      margin: const EdgeInsets.all(10),
-                                      color: Colors.blue,
-                                      width: 10,
-                                      height: 10,
-                                    )),
-                          ),
-                        ),
-                      ],
                     ),
-                  ),
+                    const Icon(
+                      AntDesign.filter,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.13),
+              child: tabBarWidget(
+                controller: ShoeMenuController,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.18,
+              ),
+              child: TabBarView(
+                controller: ShoeMenuController,
+                children: [
+                  futureBuilder(_male),
+                  futureBuilder(_female),
+                  futureBuilder(_kid),
                 ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget futureBuilder(Future<List<Sneaker>> future) {
+  return FutureBuilder<List<Sneaker>>(
+    future: future,
+    builder: (context, asyncSnapshot) {
+      if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+        return const CircularProgressIndicator();
+      } else if (asyncSnapshot.hasError) {
+        return Text(asyncSnapshot.error.toString());
+      } else {
+        final List<Sneaker> shoeData = asyncSnapshot.data!;
+        return StaggeredGridView.countBuilder(
+          padding: EdgeInsets.only(top: 0),
+          shrinkWrap: true,
+          crossAxisCount: 2,
+          crossAxisSpacing: 2,
+          mainAxisSpacing: 10,
+          itemCount: shoeData.length,
+          scrollDirection: Axis.vertical,
+          itemBuilder: ((context, index) {
+            final shoeItem = shoeData[index];
+            return Container(
+              padding:
+                  const EdgeInsets.only(top: 0, bottom: 2, left: 4, right: 6),
+              child: StaggeredTitle(
+                  shoeName: shoeItem.name,
+                  shoeType: shoeItem.category,
+                  imgUrl: shoeItem.imageUrl[1],
+                  price: (double.parse(shoeItem.price) * 83.22).round(),
+                  id: shoeItem.id),
+            );
+          }),
+          staggeredTileBuilder: (index) => StaggeredTile.extent(
+              (index % 2 == 0) ? 1 : 1,
+              (index % 3 == 1 || index % 6 == 3)
+                  ? MediaQuery.of(context).size.height * 0.33
+                  : MediaQuery.of(context).size.height * 0.30),
+        );
+      }
+    },
+  );
 }
